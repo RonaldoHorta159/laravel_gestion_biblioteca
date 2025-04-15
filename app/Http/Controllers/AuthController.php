9e89917f95bd5d44fba18825a9886105355bb925
ronaldo <72111413@continental.edu.pe>
2025-04-15 12:26:19 -0500

    Primer commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    // Redirige a Google para iniciar sesión
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    // Maneja la respuesta de Google después de la autenticación
+    public function handleGoogleCallback()
+    {
+        // Obtén el usuario de Google
+        $userGoogle = Socialite::driver('google')->stateless()->user();
+
+        // Depuración: Verifica qué contiene el objeto $userGoogle
+        // Esto imprimirá los datos del usuario y detendrá la ejecución
+
+        // Si todo está correcto, deberías obtener los datos del usuario de Google
+        $fecha = Carbon::now();
+        $user = User::updateOrCreate(
+            [
+                'google_id' => $userGoogle->id,
+            ],
+            [
+                'name' => $userGoogle->name,
+                'email' => $userGoogle->email,
+                'password' => bcrypt('957782190'), // Cambia esto por una contraseña generada o un hash
+                'email_verified_at' => $fecha,
+                'google_token' => $userGoogle->token,
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Fortify;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::get('/login/google', [AuthController::class, 'redirectToGoogle']);
+Route::get('/login/google/callback', [AuthController::class, 'handleGoogleCallback']);
